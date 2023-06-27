40e955d013fd2499ba296dbc3642dae1014273bb
Andy-DoUrden <111894438+Andy-DoUrden@users.noreply.github.com>
2023-06-27 21:39:59 +0300

    Final build fix(ghpages)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,13 +14,11 @@ export default class App extends Component {
 
   countTotalFeedback = () => {
     const { good, neutral, bad } = this.state;
-
     return good + neutral + bad;
   };
 
   countPositiveFeedbackPercentage = () => {
     const { good } = this.state;
-
     return Math.round((good / this.countTotalFeedback()) * 100);
   };
 
